a33ba40081bb9b0a99be9e23c5d83bb72a935575
Dana Awaida <miraawaida@gmail.com>
2023-10-05 11:17:10 +0300

    restart game

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+// Mira 
+const resetCardGame = document.getElementById("resetButton");
+resetCardGame.addEventListener("click", resetGame);
+
+function resetGame() {
+    window.location.reload()
+    };
+
+// Mira
